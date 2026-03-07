d97014a8f9243f6e7ee4af50f6ef2d4ea2544b57
Benin_24 <beninvijay@gmail.com>
2026-03-07 23:38:17 +0530

    Fix email port 465

--- src/services/email.js
+++ src/services/email.js
@@ -2,11 +2,14 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host: 'smtp-relay.brevo.com',
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.BREVO_USER,
     pass: process.env.BREVO_SMTP_KEY
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
